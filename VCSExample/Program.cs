90f448fc834d4a51486b6eb648989939db91e3d4
hollen <bhollen666@gmail.com>
2017-09-24 14:06:11 -0400

    Added a HalfIt function within the feature_halfing branch of the code

--- VCSExample/Program.cs
+++ VCSExample/Program.cs
@@ -11,12 +11,19 @@ namespace VCSExample
         static void Main(string[] args)
         {
             Console.WriteLine("Here's a value: {0}", DoubleIt(2));
+            Console.WriteLine("Here's a half value: {0}", HalfIt(3.14159));
             Console.WriteLine("");
         }
         static public double DoubleIt(double aInput)
         {
             double value = 0;
             value = aInput * 2.00;
+            return value;
+        }
+        static public double HalfIt(double aInput)
+        {
+            double value = 0;
+            value = aInput / 2.00;
             return value;
         }
     }
