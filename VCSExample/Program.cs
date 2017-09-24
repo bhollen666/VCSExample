f29e3724113eb60d0d7ac6cd1b872107db8d9371
hollen <bhollen666@gmail.com>
2017-09-24 14:41:57 -0400

    Added the HalfIt() function to this feature

--- VCSExample/Program.cs
+++ VCSExample/Program.cs
@@ -11,12 +11,19 @@ namespace VCSExample
         static void Main(string[] args)
         {
             Console.WriteLine("Here's a value: {0}", DoubleIt(2));
+            Console.WriteLine("Half of 0.25 is {0}", HalfIt(0.25));
             Console.WriteLine("");
         }
         static public double DoubleIt(double aInput)
         {
             double value = 0;
             value = aInput * 2.00;
+            return value;
+        }
+        static public double HalfIt(double aInput)
+        {
+            double value = 0;
+            value = aInput / 2.00;
             return value;
         }
     }
